b51bd72c41b085ad106f88f470178c7e67d5f01f
razahamza0308 <omer91999@gmail.com>
2021-04-24 00:25:57 +0500

    ss

---
+++ app.js
@@ -0,0 +1,19 @@
+// alert("Good Morning")
+
+// alert("Hello, Mr bean.\nHow are you?")
+
+// console.log("this is alert")
+
+// var message = "Hello World"
+
+// alert(message) 
+
+
+// var book= "A smarter way to learn JavaScript"
+// document.write(book)
+
+// var age = 22
+// alert("my age is"+" "+age)
+
+// var birthyear = 1998
+// document.write("My birthyear is"+" "+ birthyear)
